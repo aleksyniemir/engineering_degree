2b2dee147800d29d991db6cfa15b3963ca6fa493
Aleksander Niemirka <aleksander.niemirka@gmail.com>
2023-11-30 18:40:34 +0100

    the login on the site works and puts token in the localstorage

--- flask/app/main.py
+++ flask/app/main.py
@@ -9,5 +9,5 @@ def hello_world():
     return jsonify({"Hello":"World!"})
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run()
 
--- flask/app/models/game.py
+++ flask/app/models/game.py
@@ -1,10 +1,16 @@
+# from sqlalchemy import Integer, String, ForeignKey
+# from sqlalchemy.orm import Mapped, mapped_column
+
+# from app import db
 
 # class Game(db.Model): 
-    #__tablename__ = 'games'
-    # id = db.Column(db.Integer, primary_key = True, autoincrement=True)
-    # user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    # title = db.Column(db.String(255), nullable=False)
+#     __tablename__ = 'games'
+#     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+#     user_id: Mapped[int] = mapped_column(Integer, ForeignKey())
+#     #db.Column(db.Integer, primary_key = True, autoincrement=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+#     title = db.Column(db.String(255), nullable=False)
 
-    # user = db.relationship('User', back_populates='games')
+#     user = db.relationship('User', back_populates='games')
 
-    # turns = db.relationship('Turn', back_populates='game')
+#     # turns = db.relationship('Turn', back_populates='game')
