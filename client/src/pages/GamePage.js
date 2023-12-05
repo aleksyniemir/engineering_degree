7465e315981fcd36d5071261139881a65c143d42
Aleksander Niemirka <aleksander.niemirka@gmail.com>
2023-12-05 16:56:34 +0100

    added mock and styles to GamePage

--- client/src/pages/GamePage.js
+++ client/src/pages/GamePage.js
@@ -7,20 +7,18 @@ function GamePage() {
     return (
         <div className="game-page">
           <header className="game-header">
-            <button onClick={() => {/* handle new game */}}>New game</button>
-            <button onClick={() => {/* handle saved games */}}>Saved games</button>
-            <button onClick={() => {/* handle logout */}}>Logout</button>
+            <button class="ripple ripple-surface ripple-surface-light btn btn-dark mb-4"  onClick={() => {/* handle new game */}}>New game</button>
+            <button class="ripple ripple-surface ripple-surface-light btn btn-dark mb-4"  onClick={() => {/* handle saved games */}}>Saved games</button>
+            <button class="ripple ripple-surface ripple-surface-light btn btn-dark mb-4"  onClick={() => {/* handle logout */}}>Logout</button>
           </header>
           <main className="game-content">
             <section className="game-description">
-              <h2>Description of the situation</h2>
-              {/* Replace this with the actual game description */}
-              <p>Here goes the dynamic description of the game situation...</p>
+              <p>Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...</p>
             </section>
             <aside className="game-sidebar">
               <div className="game-photo">
                 {/* Replace with dynamic content as needed */}
-                <img src="path-to-your-photo.jpg" alt="Game" />
+                <img src={require("/home/aleksyniemir/Documents/praca_inżynierska/aplikacja/client/src/example_img.png")} alt="" />
               </div>
               <div className="game-stats">
                 <h2>Stats</h2>
@@ -28,14 +26,14 @@ function GamePage() {
               </div>
             </aside>
           </main>
-          <footer className="game-input">
-            <input
-              type="text"
-              value={userInput}
-              onChange={(e) => setUserInput(e.target.value)}
-              placeholder="User input"
-            />
-          </footer>
+          <div className="game-input"> {/* Changed from footer to div */}
+                <input
+                type="text"
+                value={userInput}
+                onChange={(e) => setUserInput(e.target.value)}
+                placeholder="Your command"
+                />
+            </div>
         </div>
       );
 
