2fc68606bcf5cb50baa2f2c76ce36c5e0122adc9
Aleksander Niemirka <aleksander.niemirka@gmail.com>
2023-12-06 16:04:35 +0100

    added new gpt key, modified the prompt, and got gpt examples

--- flask/app/gpt.py
+++ flask/app/gpt.py
@@ -1,10 +1,14 @@
 import os
-import openai
+import json
+from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(
+    organization="org-M3Ibgl6TbM8lwc9aKuANBNsz",
+    api_key = os.getenv("OPENAI_API_KEY")
+) 
 
 prompt = '''
 You are a computer program which imitates a game master, designed to output only JSON file:
@@ -30,8 +34,9 @@ Main rules:
 5. 'Scene' should be a short description of what is going on in the game right now, as if you had to describe it to a painter which is creating a picture. It should stay between 1 to 2 sentences.
 6. 'Health' is a number from 0 to 20. The player starts with 20. If it reaches 0 or below, the player dies and the game is over (He can start the game again. Game must write completly different story). The player can lose all of his health and die by doing risky stuff. The player can gain health by eating, drinking, or sleeping. 
 7. 'Weather' and 'Location' is dependent on the description.
-8. 'Quests' can be created by the game or can be gained from talking to people.
-9. 'Possible actions' are representing what the player can do next. Every action should be very short. There should be a few possible actions.
+8. 'Inventory' is a list of items that the player has. The player can gain items by picking them up or by buying them. The player can lose items by dropping them or by selling them. The player can use items by eating them, drinking them, or by using them. The player can only use items that he has in his inventory.
+9. 'Quests' can be created by the game or can be gained from talking to people.
+10. 'Possible actions' are representing what the player can do next. Every action should be very short. There should be three possible actions. It must be a list.
 
 Rules for creation of the world and the setting:
 1. The game must use the world from Frank Herbert's Dune. The game can start on any of the planets from the series.
@@ -40,11 +45,27 @@ Rules for creation of the world and the setting:
 Start the game.
 '''
 
-completion = openai.ChatCompletion.create(
+completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
     {"role": "system", "content": prompt}
     ]
 )
 
-print(completion.choices[0].message)
+message = json.loads(completion.choices[0].message.content)
+#print(completion.choices[0].message)
+
+# completion.choices[0].message.content
+# '{\n"Description": "You are a brave adventurer who has arrived on the desert planet of Arrakis, also known as Dune. The planet is known for its harsh and inhospitable conditions, with towering sand dunes and scorching heat. You have come to this unforgiving world in search of the valuable resource known as spice, which is found only on Arrakis. Spice is highly sought after and can be used for a variety of purposes, including interstellar travel and extending life. As you step out of your ship and onto the sandy surface of Dune, you take a moment to absorb the vastness of the desert before you.",\n"Scene": "The sun beats down relentlessly, casting long shadows across the shifting sand dunes. The wind howls, carrying with it the sound of sand grains swirling and scraping against each other.",\n"Health": "20/20",\n"Weather": "Hot and dry",\n"Location": "Arrakis (Dune)",\n"Inventory": "Empty",\n"Quests": "None",\n"Possible actions": "Explore the surroundings, look for a settlement, search for water"\n}'
+
+# completion_1 = client.chat.completions.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#     {"role": "system", "content": prompt},
+#     {"role": "assistant", "content": '{\n"Description": "You are a brave adventurer who has arrived on the desert planet of Arrakis, also known as Dune. The planet is known for its harsh and inhospitable conditions, with towering sand dunes and scorching heat. You have come to this unforgiving world in search of the valuable resource known as spice, which is found only on Arrakis. Spice is highly sought after and can be used for a variety of purposes, including interstellar travel and extending life. As you step out of your ship and onto the sandy surface of Dune, you take a moment to absorb the vastness of the desert before you.",\n"Scene": "The sun beats down relentlessly, casting long shadows across the shifting sand dunes. The wind howls, carrying with it the sound of sand grains swirling and scraping against each other.",\n"Health": "20/20",\n"Weather": "Hot and dry",\n"Location": "Arrakis (Dune)",\n"Inventory": "Empty",\n"Quests": "None",\n"Possible actions": "Explore the surroundings, look for a settlement, search for water"\n}'},
+#     {"role": "user", "content": "try to find a sand worm"}
+#     ]
+# )
+
+# '{\n"Description": "As you set out to find a sand worm on the desert planet of Arrakis, you brace yourself for the dangerous task ahead. Sand worms are massive creatures that burrow deep within the desert sands, and they are the source of much fear and fascination among the inhabitants of Dune. These colossal creatures can reach incredible sizes, with some measuring hundreds of meters in length. As you venture deeper into the desert, you keep your senses alert, hoping to catch a glimpse of one of these legendary creatures.",\n"Scene": "You find yourself surrounded by endless rolling sand dunes, their golden hues shifting and shimmering in the harsh sunlight. The wind carries the faint sound of distant rumbling beneath the surface, hinting at the presence of the mighty sand worms.",\n"Health": "20/20",\n"Weather": "Hot and dry",\n"Location": "Arrakis (Dune)",\n"Inventory": "Empty",\n"Quests": "None",\n"Possible actions": "Continue exploring, search for worm tracks, create vibrations to attract a sand worm"\n}'
+# {'Description': 'As you set out to find a sand worm on the desert planet of Arrakis, you brace yourself for the dangerous task ahead. Sand worms are massive creatures that burrow deep within the desert sands, and they are the source of much fear and fascination among the inhabitants of Dune. These colossal creatures can reach incredible sizes, with some measuring hundreds of meters in length. As you venture deeper into the desert, you keep your senses alert, hoping to catch a glimpse of one of these legendary creatures.', 'Scene': 'You find yourself surrounded by endless rolling sand dunes, their golden hues shifting and shimmering in the harsh sunlight. The wind carries the faint sound of distant rumbling beneath the surface, hinting at the presence of the mighty sand worms.', 'Health': '20/20', 'Weather': 'Hot and dry', 'Location': 'Arrakis (Dune)', 'Inventory': 'Empty', 'Quests': 'None', 'Possible actions': 'Continue exploring, search for worm tracks, create vibrations to attract a sand worm'}
