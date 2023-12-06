b2a3ae126551ceb75fa2baf01d00d0288e4117f2
Aleksander Niemirka <aleksander.niemirka@gmail.com>
2023-12-06 13:24:50 +0100

    final app layout

--- client/src/pages/GamePage.js
+++ client/src/pages/GamePage.js
@@ -5,36 +5,36 @@ function GamePage() {
     const [userInput, setUserInput] = useState('');
 
     return (
-        <div className="game-page">
-          <header className="game-header">
-            <button class="ripple ripple-surface ripple-surface-light btn btn-dark mb-4"  onClick={() => {/* handle new game */}}>New game</button>
-            <button class="ripple ripple-surface ripple-surface-light btn btn-dark mb-4"  onClick={() => {/* handle saved games */}}>Saved games</button>
-            <button class="ripple ripple-surface ripple-surface-light btn btn-dark mb-4"  onClick={() => {/* handle logout */}}>Logout</button>
-          </header>
-          <main className="game-content">
-            <section className="game-description">
-              <p>Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...</p>
-            </section>
-            <aside className="game-sidebar">
-              <div className="game-photo">
-                {/* Replace with dynamic content as needed */}
-                <img src={require("/home/aleksyniemir/Documents/praca_inżynierska/aplikacja/client/src/example_img.png")} alt="" />
+      <body class="game-page">
+        <header className="game-header">
+          <button class="ripple ripple-surface ripple-surface-light btn btn-dark mb-4"  onClick={() => {}}>New game</button>
+          <button class="ripple ripple-surface ripple-surface-light btn btn-dark mb-4"  onClick={() => {}}>Saved games</button>
+          <button class="ripple ripple-surface ripple-surface-light btn btn-dark mb-4"  onClick={() => {}}>Logout</button>
+        </header>
+        <div class="container">
+            <div class="left-container">
+                <div className="game-description">
+                  <p>Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...Here goes the dynamic description of the game situation...</p>
+                </div>
+              <div class="bottom-container">
+                <div className="game-input"> 
+                  <input
+                    type="text"
+                    value={userInput}
+                    onChange={(e) => setUserInput(e.target.value)}
+                    placeholder="Your command"
+                  />
+                </div>
+                <div className="game-photo">
+                  <img src={require("/home/aleksyniemir/Documents/praca_inżynierska/aplikacja/client/src/example_img.png")} alt="" />
+                </div>
               </div>
-              <div className="game-stats">
-                <h2>Stats</h2>
-                {/* Stats content goes here */}
-              </div>
-            </aside>
-          </main>
-          <div className="game-input"> {/* Changed from footer to div */}
-                <input
-                type="text"
-                value={userInput}
-                onChange={(e) => setUserInput(e.target.value)}
-                placeholder="Your command"
-                />
             </div>
-        </div>
+            <div className="game-stats">
+                  <h2>Stats</h2>
+                </div>
+            </div>
+        </body>
       );
 
 }
