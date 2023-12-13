fee744dd7207983df470cd44dd7b6bc6da57aaa4
Aleksander Niemirka <aleksander.niemirka@gmail.com>
2023-12-13 22:19:09 +0100

    modified game model to have a photo

--- flask/app/models/game.py
+++ flask/app/models/game.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy import Integer, String, ForeignKey, LargeBinary
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app import db
@@ -8,6 +8,7 @@ class Game(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     title: Mapped[str]
+    photo: Mapped[LargeBinary] = mapped_column(LargeBinary)
 
     user: Mapped["User"] = relationship("User", back_populates="games")
 
