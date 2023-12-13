242b16a594aaf06aca8d0827d69028d9ae09f765
Aleksander Niemirka <aleksander.niemirka@gmail.com>
2023-12-13 23:11:15 +0100

    modified turn and game models

--- flask/app/models/game.py
+++ flask/app/models/game.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Integer, String, ForeignKey, LargeBinary
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 from app import db
 
@@ -9,8 +10,17 @@ class Game(db.Model):
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     title: Mapped[str]
     photo: Mapped[LargeBinary] = mapped_column(LargeBinary)
+    prompt: Mapped[str] = mapped_column(String(2047))
+    description: Mapped[str] = mapped_column(String(1023))
+    scene: Mapped[str] = mapped_column(String(255))
+    turn_number: Mapped[int]    
+    possible_actions: Mapped[str] = mapped_column(String(255))
+    quests: Mapped[str] = mapped_column(String(255))
+    inventory: Mapped[str] = mapped_column(String(255))
+    location: Mapped[str] = mapped_column(String(255))
+    weather: Mapped[str] = mapped_column(String(255))
+    health: Mapped[str] = mapped_column(String(255))
 
     user: Mapped["User"] = relationship("User", back_populates="games")
 
-    # turns = db.relationship('Turn', back_populates='game')
 from app.models.user import User
--- flask/app/models/turn.py
+++ flask/app/models/turn.py
@@ -1,9 +1,14 @@
+# from sqlalchemy.orm import Mapped, mapped_column, relationship
+# from sqlalchemy import Integer, String, ForeignKey
+
+# from app import db
+
 # class Turn(db.Model):
 #     __tablename__ = 'turns'
-#     id = db.Column(db.Integer, primary_key = True, autoincrement=True)
-#     game_id = db.column(db.Integer, db.ForeignKey('games.id'), nullable=False)
-#     body = db.Column(db.String(1023), nullable=False)
-#     picture = db.Column(db.LargeBinary, nullable=True)
-#     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+#     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+#     game_id: Mapped[int] = mapped_column(ForeignKey("games.id"))
+    
 
-#     game = db.relationship('Game', back_populates='turns')
+#     game: Mapped["Game"] = relationship("Game", back_populates="turns")
+
+# from app.models.game import Game
