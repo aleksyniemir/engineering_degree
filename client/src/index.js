aaec7fb029034bf42ab9dd97ab260d7239b5d7a5
Aleksander Niemirka <aleksander.niemirka@gmail.com>
2024-02-04 16:48:07 +0100

    cleaned code

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-// the entry point of your React application. It's typically responsible for 
-// rendering the root React component (App) into the DOM and includes setup 
-// for things like Redux or other global libraries.
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
